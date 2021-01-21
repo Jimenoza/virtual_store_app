3e2aec5343163139073316a0e21c6da1d218e1b6
LuisPa <luispama96@hotmail.com>
2021-01-20 20:31:27 -0600

    Search screen

--- src/screens/product/containers/product-list.js
+++ src/screens/product/containers/product-list.js
@@ -2,182 +2,7 @@ import React, {Component} from 'react';
 import { FlatList } from 'react-native';
 import Product from '../components/product'
 
-const products = {
-    "data": {
-        "current_page": 1,
-        "data": [
-            {
-                "id": 1,
-                "name": "Bulto Jansport",
-                "description": "Bulto azul con fondo café",
-                "image": "http://localhost:8000/images/productos/1602557763.jpg",
-                "price": 15,
-                "stock": 18,
-                "available": 1,
-                "califications": 1,
-                "average": 5,
-                "category_id": 1,
-                "category_name": "Escolar"
-            },
-            {
-                "id": 2,
-                "name": "Billetera",
-                "description": "Billetera para hombre",
-                "image": "http://localhost:8000/images/productos/1601436540.jpg",
-                "price": 10,
-                "stock": 31,
-                "available": 1,
-                "califications": 0,
-                "average": 0,
-                "category_id": 4,
-                "category_name": "Accesorios"
-            },
-            {
-                "id": 3,
-                "name": "Nintendo Switch",
-                "description": "La última consola de nintendo",
-                "image": "http://localhost:8000/images/productos/1601442531.jpg",
-                "price": 299,
-                "stock": 0,
-                "available": 1,
-                "califications": 0,
-                "average": 0,
-                "category_id": 3,
-                "category_name": "Consolas"
-            },
-            {
-                "id": 4,
-                "name": "Fallout 4",
-                "description": "Un mundo postapocalíptico después de una guerra nuclear",
-                "image": "http://localhost:8000/images/productos/1603339114.webp",
-                "price": 45,
-                "stock": 18,
-                "available": 1,
-                "califications": 0,
-                "average": 0,
-                "category_id": 2,
-                "category_name": "Videojuegos"
-            },
-            {
-                "id": 5,
-                "name": "The last of us",
-                "description": "Juego del año 2013",
-                "image": "http://localhost:8000/images/productos/1601437419.jpg",
-                "price": 19.99,
-                "stock": 18,
-                "available": 1,
-                "califications": 0,
-                "average": 0,
-                "category_id": 2,
-                "category_name": "Videojuegos"
-            },
-            {
-                "id": 7,
-                "name": "Lentes",
-                "description": "anteojos unisex",
-                "image": "http://localhost:8000/images/productos/1601437471.jpg",
-                "price": 15,
-                "stock": 3,
-                "available": 1,
-                "califications": 0,
-                "average": 0,
-                "category_id": 4,
-                "category_name": "Accesorios"
-            },
-            {
-                "id": 10,
-                "name": "Wii U",
-                "description": "Una consola que parecía un control para la wii",
-                "image": "http://localhost:8000/images/productos/1602301594.png",
-                "price": 110,
-                "stock": 5,
-                "available": 1,
-                "califications": 0,
-                "average": 0,
-                "category_id": 3,
-                "category_name": "Consolas"
-            },
-            {
-                "id": 11,
-                "name": "Zelda Breath of the Wild",
-                "description": "Último juego de la Saga The Legende of Zelda. Mejor juego del año 2017",
-                "image": "http://localhost:8000/images/productos/1601437528.jpg",
-                "price": 59.99,
-                "stock": 27,
-                "available": 1,
-                "califications": 1,
-                "average": 5,
-                "category_id": 2,
-                "category_name": "Videojuegos"
-            },
-            {
-                "id": 12,
-                "name": "Cuadernos",
-                "description": "Cuardenos multicolor para escuela, colegio, universidad",
-                "image": "http://localhost:8000/images/productos/1601437562.jpg",
-                "price": 12,
-                "stock": 49,
-                "available": 1,
-                "califications": 1,
-                "average": 5,
-                "category_id": 1,
-                "category_name": "Escolar"
-            },
-            {
-                "id": 13,
-                "name": "Audífonos",
-                "description": "Audífonos negros grandes",
-                "image": "http://localhost:8000/images/productos/1601437588.jpg",
-                "price": 14.99,
-                "stock": 7,
-                "available": 1,
-                "califications": 2,
-                "average": 2,
-                "category_id": 4,
-                "category_name": "Accesorios"
-            }
-        ],
-        "first_page_url": "http://localhost:8000/api/products?page=1",
-        "from": 1,
-        "last_page": 2,
-        "last_page_url": "http://localhost:8000/api/products?page=2",
-        "links": [
-            {
-                "url": null,
-                "label": "&laquo; Anterior",
-                "active": false
-            },
-            {
-                "url": "http://localhost:8000/api/products?page=1",
-                "label": 1,
-                "active": true
-            },
-            {
-                "url": "http://localhost:8000/api/products?page=2",
-                "label": 2,
-                "active": false
-            },
-            {
-                "url": "http://localhost:8000/api/products?page=2",
-                "label": "Siguiente &raquo;",
-                "active": false
-            }
-        ],
-        "next_page_url": "http://localhost:8000/api/products?page=2",
-        "path": "http://localhost:8000/api/products",
-        "per_page": 10,
-        "prev_page_url": null,
-        "to": 10,
-        "total": 17
-    },
-    "error": null
-}
-
 class ProductList extends Component{
-    state = {
-        products: products.data.data,
-        selected_product: null,
-    };
     goToDetails = (item) => {
         this.props.navigation.push('Details',{product_id : item.id});
     }
@@ -188,11 +13,16 @@ class ProductList extends Component{
         );
     }
     render(){
-        return <FlatList
-            keyExtractor={this.keyExtractor}
-            data={this.state.products}
-            renderItem={this.renderItem}
-        />
+        if(this.props.items){
+            return <FlatList
+                keyExtractor={this.keyExtractor}
+                data={this.props.items}
+                renderItem={this.renderItem}
+            />
+        }
+        else{
+            return null;
+        }
     }
 }
 
--- src/screens/search/containers/search-screen.js
+++ src/screens/search/containers/search-screen.js
@@ -1,10 +1,117 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, StyleSheet, ActivityIndicator } from 'react-native';
+import ProductList from '../../product/containers/product-list';
+
+const results = {
+    "data": [
+        {
+            "id": 5,
+            "name": "The last of us",
+            "description": "Juego del año 2013",
+            "image": "http://localhost:8000/images/productos/1601437419.jpg",
+            "price": 19.99,
+            "stock": 18,
+            "available": 1,
+            "califications": 0,
+            "average": 0,
+            "category_id": 2
+        },
+        {
+            "id": 11,
+            "name": "Zelda Breath of the Wild",
+            "description": "Último juego de la Saga The Legende of Zelda. Mejor juego del año 2017",
+            "image": "http://localhost:8000/images/productos/1601437528.jpg",
+            "price": 59.99,
+            "stock": 27,
+            "available": 1,
+            "califications": 1,
+            "average": 5,
+            "category_id": 2
+        },
+        {
+            "id": 19,
+            "name": "The Last of Us Part II",
+            "description": "Secuela del primer juego",
+            "image": "http://localhost:8000/images/productos/1601437723.jpg",
+            "price": 59.99,
+            "stock": 8,
+            "available": 1,
+            "califications": 1,
+            "average": 4,
+            "category_id": 2
+        }
+    ],
+    "error": null
+}
 
 class SearchScreen extends Component{
-    render(){
-        return <View><Text>Buscador</Text></View>
+    state = {
+        products: null,
+        searching: false,
+    };
+    
+    search(event){
+        if(event){
+            this.setState( {
+                searching : true,
+                products: results,
+            });
+        }
+        if(!event){
+            this.setState( {
+                searching : false,
+                products: null,
+            });
+        }
     }
+
+    renderResults(){
+        if(this.state.products){
+            return <ProductList {...this.props} items={this.state.products.data}/>
+        }
+    }
+
+    render(){
+        return (
+            <View style={styles.screen_container}>
+                <View style={styles.search_container}>
+                    <TextInput autoFocus={true} style={styles.box} placeholder='Buscar...' onChangeText={(ev) => this.search(ev)}/>
+                </View>
+                <View style={styles.display}>
+                    <ActivityIndicator size='large' color="#0e8ce4" animating={this.state.searching}/>
+                    {this.renderResults()}
+                </View>
+            </View>
+        )
+    }
+
+    
 }
+
+const styles = StyleSheet.create({
+    screen_container : {
+        backgroundColor : '#eff6fa',
+        flex: 1,
+    },
+    box : {
+        backgroundColor: 'white',
+        height: 30,
+        padding: 0,
+        marginLeft: 10,
+        marginRight: 10,
+        borderRadius: 10,
+        paddingLeft: 10,
+    },
+    search_container: {
+        backgroundColor: '#e3e3e3',
+        alignSelf: 'stretch',
+        height: 50,
+        flexDirection: 'column',
+        justifyContent: 'center'
+    },
+    display : {
+        alignItems: 'center',
+    }
+});
 
 export default SearchScreen;
